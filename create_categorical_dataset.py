f21947f4290238c4e878500145df49e25fc0d0f2
ranggarmaste <ranggarmaste@gmail.com>
2017-12-02 01:17:46 +0700

    Small fix

--- create_categorical_dataset.py
+++ create_categorical_dataset.py
@@ -23,6 +23,7 @@ for i in categorical_indices:
     X[:, i] = labelencoder_X.fit_transform(X[:, i])
     
 #%%
+    
 from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
 X[:, numeric_indices] = sc_X.fit_transform(X[:, numeric_indices])
@@ -31,11 +32,13 @@ onehotencoder_X = OneHotEncoder(categorical_features=categorical_indices)
 X = onehotencoder_X.fit_transform(X).toarray()
 
 #%%
+
 y = data.iloc[:, 14].values
 y = y != ' <=50K'
 y = y.astype(int)
 
 #%%
+
 file = open('dataset_m', 'w')
 file.write('{}\n'.format(len(X)))
 for i in range(len(X)):
@@ -43,3 +46,15 @@ for i in range(len(X)):
     file.write(' {}'.format(y[i]))
     file.write('\n')
 file.close()
+
+#%%
+
+# Optional
+# from sklearn.manifold import TSNE
+
+# tsne = TSNE(n_components=2, verbose=1, perplexity=40, n_iter=300)
+# tsne_results = tsne.fit_transform(X)
+
+# from matplotlib import pyplot as plt
+# plt.scatter(tsne_results[:, 0], tsne_results[:, 1], c=y)
+# plt.show()
